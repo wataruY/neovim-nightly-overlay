bebd3762482897b245ed3a20668627996dd27679
wataruY <wataru.yamazaki.nullptr@gmail.com>
2025-06-14 15:55:51 +0900

    fix: update tree sitter hash

--- flake/packages/neovim.nix
+++ flake/packages/neovim.nix
@@ -56,7 +56,7 @@ let
               // {
                 version = "bundled";
                 src = deps.treesitter;
-                cargoHash = "sha256-rjUn8F6WSxLQGrFzK23q4ClLePSpcMN2+i7rC02Fisk=";
+                cargoHash = "sha256-sGh16M7localcbT5ct1sT2FcUUoIQFcoOftTuQ0aSCjtkTEs=";
               }
             );
         };
